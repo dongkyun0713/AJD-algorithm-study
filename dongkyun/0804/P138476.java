b19e2fe0d1b17650d902b2443e1947bea640930f
dongkyun <kyun010713@gmail.com>
2023-08-04 03:17:57 +0900

    [P2C] P138476

--- dongkyun/0804/P138476.java
+++ dongkyun/0804/P138476.java
@@ -7,13 +7,12 @@ import java.util.Iterator;
 
 public class P138476 {
     public int solution(int k, int[] tangerine) {
-
+        //[1, 3, 2, 5, 4, 5, 2, 3]
         int answer = 0;
         int count = 1;
-        // 1, 3, 2, 5, 4, 5, 2, 3
+        Arrays.sort(tangerine);
         // 1, 2, 2, 3, 3, 4, 5, 5
         ArrayList<Integer> a = new ArrayList<>();
-
         for(int i =0; i < tangerine.length; i++) {  // 같은 값이 나온 횟수를 arraylist에 저장
             if(i == tangerine.length-1) {
                 a.add(count);
@@ -30,13 +29,12 @@ public class P138476 {
         // 리스트a {1, 2, 2, 1, 2)
         Collections.sort(a, Collections.reverseOrder());  // 내림차순 정렬{2, 2, 2, 1, 1}
         Iterator<Integer> it = a.iterator();
-
         while (it.hasNext()){
             k -= it.next();
             answer++;
             if(k<=0) break;
-        }  // 1, 1, 1, 1, 2, 2, 2, 3
-
+        }
+        if(answer ==0) return 1;
         return answer;
     }
 }
